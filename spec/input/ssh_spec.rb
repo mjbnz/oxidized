b5282ee0376e1f4a69bf799ba465a3de0edd75e1
Mike Beattie <mike@ethernal.org>
2022-11-08 11:41:39 +1300

    Update ssh_spec.rb

--- spec/input/ssh_spec.rb
+++ spec/input/ssh_spec.rb
@@ -15,13 +15,15 @@ describe Oxidized::SSH do
   describe "#connect" do
     it "should use proxy command when proxy host given and connect by ip if resolve_dns is true" do
       Oxidized.config.resolve_dns = true
-      @node = Oxidized::Node.new(name:     'example.com',
-                                 input:    'ssh',
-                                 output:   'git',
-                                 model:    'junos',
-                                 username: 'alma',
-                                 password: 'armud',
-                                 vars:     { ssh_proxy: 'test.com' })
+      @node = Oxidized::Node.new(
+        name:     'example.com',
+        input:    'ssh',
+        output:   'git',
+        model:    'junos',
+        username: 'alma',
+        password: 'armud',
+        vars:     { ssh_proxy: 'test.com' }
+      )
 
       ssh = Oxidized::SSH.new
 
@@ -31,15 +33,17 @@ describe Oxidized::SSH do
 
       proxy = mock
       Net::SSH::Proxy::Command.expects(:new).with("ssh test.com -W [%h]:%p").returns(proxy)
-      Net::SSH.expects(:start).with('93.184.216.34', 'alma',  port:                             22,
-                                                              verify_host_key:                  Oxidized.config.input.ssh.secure ? :always : :never,
-                                                              append_all_supported_algorithms:  true,
-                                                              keepalive:                        true,
-                                                              password:                         'armud',
-                                                              timeout:                          Oxidized.config.timeout,
-                                                              number_of_password_prompts:       0,
-                                                              auth_methods:                     %w[none publickey password],
-                                                              proxy:                            proxy)
+      Net::SSH.expects(:start).with('93.184.216.34', 'alma',
+        port:                             22,
+        verify_host_key:                  Oxidized.config.input.ssh.secure ? :always : :never,
+        append_all_supported_algorithms:  true,
+        keepalive:                        true,
+        password:                         'armud',
+        timeout:                          Oxidized.config.timeout,
+        number_of_password_prompts:       0,
+        auth_methods:                     %w[none publickey password],
+        proxy:                            proxy
+      )
 
       ssh.instance_variable_set("@exec", true)
       ssh.connect(@node)
@@ -47,13 +51,15 @@ describe Oxidized::SSH do
 
     it "should use proxy command when proxy host given and connect by name if resolve_dns is false" do
       Oxidized.config.resolve_dns = false
-      @node = Oxidized::Node.new(name:     'example.com',
-                                 input:    'ssh',
-                                 output:   'git',
-                                 model:    'junos',
-                                 username: 'alma',
-                                 password: 'armud',
-                                 vars:     { ssh_proxy: 'test.com' })
+      @node = Oxidized::Node.new(
+        name:     'example.com',
+        input:    'ssh',
+        output:   'git',
+        model:    'junos',
+        username: 'alma',
+        password: 'armud',
+        vars:     { ssh_proxy: 'test.com' }
+      )
 
       ssh = Oxidized::SSH.new
 
@@ -63,15 +69,19 @@ describe Oxidized::SSH do
 
       proxy = mock
       Net::SSH::Proxy::Command.expects(:new).with("ssh test.com -W [%h]:%p").returns(proxy)
-      Net::SSH.expects(:start).with('example.com', 'alma',  port:                             22,
-                                                            verify_host_key:                  Oxidized.config.input.ssh.secure ? :always : :never,
-                                                            append_all_supported_algorithms:  true,
-                                                            keepalive:                        true,
-                                                            password:                         'armud',
-                                                            timeout:                          Oxidized.config.timeout,
-                                                            number_of_password_prompts:       0,
-                                                            auth_methods:                     %w[none publickey password],
-                                                            proxy:                            proxy)
+      Net::SSH.expects(:start).with(
+        'example.com',
+        'alma',
+        port:                             22,
+        verify_host_key:                  Oxidized.config.input.ssh.secure ? :always : :never,
+        append_all_supported_algorithms:  true,
+        keepalive:                        true,
+        password:                         'armud',
+        timeout:                          Oxidized.config.timeout,
+        number_of_password_prompts:       0,
+        auth_methods:                     %w[none publickey password],
+        proxy:                            proxy
+      )
 
       ssh.instance_variable_set("@exec", true)
       ssh.connect(@node)
